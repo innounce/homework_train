7736c4bad0442f2bb6a9fc4a2488059f095620fd
edward <edwardlin@wezoomtek.com.tw>
2019-10-16 00:37:25 +0800

    inherient

--- src/main/kotlin/Student/Student.kt
+++ src/main/kotlin/Student/Student.kt
@@ -7,20 +7,43 @@ fun main(args: Array<String>) {
 
 //    userInput()
     val stu = Student("Jack", 66, 88)
+    val stu2 = Student("Eric", 66, 44)
+    val stu3 = Student("Luie", 66, 22)
     stu.Print()
-//    stu.getAverage()
-    println("high score: ${stu.hightest()}")
+    stu2.Print()
+    stu3.Print()
+//    println("high score: ${stu.hightest()}")
+    Student.PASS = 50
+    stu.Print()
+    stu2.Print()
+    stu3.Print()
+
+    val gstu = GraduadeStudent("Edward", 50,55,60)
+    gstu.Print()
 
 }
+class GraduadeStudent(name: String?, math: Int, english: Int, val thesis: Int) : Student(name, math, english) {
+    companion object {
+        var PASS = 70
+    }
 
+    override fun Print() {
+        println("$name\t$math\t$english\t$thesis\t${getAverage()}\t${passOrFailed()}\t${getGrade()}")
+    }
 
-class Student(var name: String?, var math: Int, var english:Int){
+    override fun passOrFailed() = if( getAverage() >= PASS) "PASS" else "FAILED"
+}
 
-    fun Print(){
+open class Student(var name: String?, var math: Int, var english:Int){
+    companion object {
+        var PASS = 60
+    }
+
+    open fun Print(){
         println("$name\t$math\t$english\t${getAverage()}\t${passOrFailed()}\t${getGrade()}")
     }
 
-    fun passOrFailed() = if( getAverage() >=60 ) "PASS" else "FAILED"
+    open fun passOrFailed() = if( getAverage() >= PASS ) "PASS" else "FAILED"
 
     fun getGrade() = when ( getAverage() ) {
         in 90..100 -> 'A'
